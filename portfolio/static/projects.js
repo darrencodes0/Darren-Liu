ea188eb7791932fbbf93680379ccf0f82459bec5
Darren Liu <darrenliu101@gmail.com>
2025-01-19 18:47:21 -0500

    portfolio

---
+++ portfolio/static/projects.js
@@ -0,0 +1,39 @@
+const sliders = {
+  slider1: { currentIndex: 0 },
+  slider2: { currentIndex: 0 },
+  slider3: { currentIndex: 0 },
+};
+
+function slide(direction, sliderId) {
+  const slider = sliders[sliderId]; 
+  const imagesContainer = document.querySelector(`#${sliderId} .slider-images`);
+  const images = document.querySelectorAll(`#${sliderId} .slider-images img`);
+  const totalImages = images.length;
+
+  
+  slider.currentIndex += direction;
+
+  
+  if (slider.currentIndex < 0) {
+    slider.currentIndex = totalImages - 1;
+  } else if (slider.currentIndex >= totalImages) {
+    slider.currentIndex = 0;
+  }
+
+  const containerWidth = imagesContainer.parentElement.clientWidth;
+  const offset = -slider.currentIndex * containerWidth;
+  
+  imagesContainer.style.transform = `translateX(${offset}px)`;
+}
+
+
+window.addEventListener('resize', () => {
+  for (const sliderId in sliders) {
+    const slider = sliders[sliderId];
+    const imagesContainer = document.querySelector(`#${sliderId} .slider-images`);
+    const containerWidth = imagesContainer.parentElement.clientWidth;
+    const offset = -slider.currentIndex * containerWidth;
+
+    imagesContainer.style.transform = `translateX(${offset}px)`;
+  }
+});
